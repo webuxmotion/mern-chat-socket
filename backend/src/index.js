3c00f7e284776019e20e208f75490ebb80669265
webuxmotion <pereverziev.andrii@gmail.com>
2025-06-20 22:57:19 +0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+    connectDB()
+})
