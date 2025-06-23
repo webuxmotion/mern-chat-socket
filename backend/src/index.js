76dc5e60b771646babe3070581dd38879ff25ac8
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-06-24 00:44:26 +0300

    deployment code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,42 @@
-import express from "express"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import { connectDB } from "./lib/db.js"
-import { app, server } from "./lib/socket.js"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-dotenv.config()
+import path from "path";
 
-const PORT = process.env.PORT
+import { connectDB } from "./lib/db.js";
 
-app.use(express.json())
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-    connectDB()
-})
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+});
